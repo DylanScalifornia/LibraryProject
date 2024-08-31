eafb018ddaad5b55309d6bf4327b9179dfd55cff
DylanScalifornia <dylanscalifornia@gmail.com>
2024-08-31 14:11:50 -0700

    worked on html and some style for library project

---
+++ script.js
@@ -0,0 +1,18 @@
+const myLibrary = [ ];
+
+function Book(title, author, pages, read) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+    this.info = function() {return `The ${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`}
+};
+
+function addBookToLibrary() {
+  
+};
+
+
+
+
+
